497778a4d4c3a18cc4c29fcf77844a37af4cae02
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-11-19 19:43:43 -0800

    Added multiple choice question

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-mult_choice = "Please enter a-e as an answer"
-num_response = "Please enter a number"
+mult_choice = "Please enter a-d as an answer"
+num_response = "Please enter a whole number"
 true_or_false = "Please enter 1 for true and 0 for false"
 
 questions_wrong = []
@@ -9,14 +9,14 @@ total_score = 0
 def correct_answer():
     global total_score
     total_score += 1
-    print("Correct!")
+    print("Correct!\n")
 
 def wrong_answer(question_number, answer):
     questions_wrong.append(str(question_number))
-    print(f"Incorrect. Correct answer: {answer}")
+    print(f"Incorrect. Correct answer: {answer}\n")
 
 def question1():
-    question = "How many minutes do sun rays take to reach earth?"
+    question = "1. How many minutes do sun rays take to reach earth?"
     answer = 8
 
     print(f"{question} {num_response}")
@@ -29,7 +29,7 @@ def question1():
         wrong_answer(1, answer)
 
 def question2():
-    question = "How many days does it take for the moon to orbit Earth?"
+    question = "2. How many days does it take for the moon to orbit Earth?"
     answer = 27
 
     print(f"{question} {num_response}")
@@ -41,8 +41,58 @@ def question2():
     else:
         wrong_answer(2, answer)
 
-question1()
-question2()
+def question3():
+    question = "3. Which planet has the most volcanoes?"
+    answer = "b"
+    letters = ["a", "b", "c", "d"]
+    choices = ["Earth", "Venus", "Jupiter", "Saturn"]
+
+    print(f"{question} {mult_choice}")
+
+    for index in range(len(choices)):
+        print(f"{letters[index]} - {choices[index]}")
+
+    guess = input().lower()
+
+    if guess in letters:
+        if guess == answer:
+            correct_answer()
+        else:
+            wrong_answer(3, answer)
+    else:
+        print(f"{guess} is not a valid option. Try again\n")
+        question3()
+
+def question4():
+    question = "4. What is the diameter of Earth?"
+    answer = "c"
+    letters = ["a", "b", "c", "d"]
+    choices = ["3,144 miles", "9,227 miles", "7,918 miles", "4,916 miles"]
+
+    print(f"{question} {mult_choice}")
+
+    for index in range(len(choices)):
+        print(f"{letters[index]} - {choices[index]}")
+
+    guess = input().lower()
+
+    if guess in letters:
+        if guess == answer:
+            correct_answer()
+        else:
+            wrong_answer(4, answer)
+    else:
+        print(f"{guess} is not a valid option. Try again\n")
+        question4()
+
+def quiz():
+    question1()
+    question2()
+    question3()
+    question4()
+
+if __name__ == "__main__":
+    quiz()
 
 print(questions_wrong)
 print(total_score)
